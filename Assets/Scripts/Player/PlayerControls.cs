c666e27e1861526930ff6f0f98bcb9f0645d8fde
Jeremie Corpinot <jeremiecorpinot@outlook.com>
2017-12-26 22:32:47 +0100

    Initialization

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float movespeed;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        movespeed = 3;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+    }
+}
