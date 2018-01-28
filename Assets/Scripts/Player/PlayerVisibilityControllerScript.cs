ca4cc9bc78aad9641fa6b385ed2c6b923e319fc4
emmanuelJd <elisa.emmanuel.jd@gmail.com>
2018-01-28 21:43:22 +0100

    start to work on purger

---
+++ Assets/Scripts/Player/PlayerVisibilityControllerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVisibilityControllerScript : MonoBehaviour {
+
+    [Range(0,100)]
+    public float visibility;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
