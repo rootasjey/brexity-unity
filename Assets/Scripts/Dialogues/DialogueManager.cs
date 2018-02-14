284fdcffb4a8dc9e92bd5f2c78766c5f0a3e9db6
rootasjey <jeremiecorpinot@outlook.com>
2018-02-14 21:52:46 +0100

    Add assets (items, voices, videos)

---
+++ Assets/Scripts/Dialogues/DialogueManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public string continueText;
+    public string closeText;
+
+    public Text nameText;
+    public Text dialogueText;
+    public Image dialogueImage;
+    public Button dialogueContinueButton;
+    public GameObject dialogueBox;
+
+    private Queue<string> sentences;
+
+    // Use this for initialization
+    void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialogueBox.SetActive(true);
+        dialogueAppear(false, 0f);
+        dialogueAppear(true, 0.5f);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach( string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentences();
+        dialogueImage.CrossFadeAlpha(1f, 1f, true);
+    }
+
+    public void DisplayNextSentences()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void OnGUI()
+    {
+        Debug.Log(sentences.Count);
+        if (sentences.Count == 0)
+        {
+            dialogueContinueButton.GetComponentInChildren<Text>().text = closeText;
+        }
+        else if (sentences.Count > 0)
+        {
+            dialogueContinueButton.GetComponentInChildren<Text>().text = continueText;
+        }
+    }
+
+    void EndDialogue()
+    {
+        dialogueAppear(false, 0.5f);
+        dialogueBox.SetActive(false);
+    }
+
+    void dialogueAppear(bool appear, float fadeTime)
+    {
+        if(appear)
+        {
+            nameText.CrossFadeAlpha(1f, fadeTime, true);
+            dialogueText.CrossFadeAlpha(1f, fadeTime, true);
+            dialogueImage.CrossFadeAlpha(1f, fadeTime, true);
+            dialogueContinueButton.GetComponent<Image>().CrossFadeAlpha(1f, fadeTime, true);
+            dialogueContinueButton.GetComponentInChildren<Text>().CrossFadeAlpha(1f, fadeTime, true);
+        }
+        else
+        {
+            nameText.CrossFadeAlpha(0f, fadeTime, true);
+            dialogueText.CrossFadeAlpha(0f, fadeTime, true);
+            dialogueImage.CrossFadeAlpha(0f, fadeTime, true);
+            dialogueContinueButton.GetComponent<Image>().CrossFadeAlpha(0f, fadeTime, true);
+            dialogueContinueButton.GetComponentInChildren<Text>().CrossFadeAlpha(0f, fadeTime, true);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# brexity
-unity game
+# Brexity 2031
+
+Brexity 2031 is a game in which you'll control an immigrant trapped at London in 2031.
+
+This is a stealth/adventure game.
+
+## Screenshots
+
+![brexity2031](brexity.png)
+
+## Teaser
+
+https://youtu.be/Lhit904ipqE
+
+## Quickstart
+
+1. Install [Unity](http://unity3d.com/)
+2. Open the project
+3. Build with Unity or Visual Studio
+4. Enjoy \o/
+
+## TODO
+
+* Enemies player detection
+* Dialog system
+* Quests system
+* Companion system
+
+## Contribute
+
+You can contribute to this game by opening pull requests,
+testing the game on your computer, modify it.
+
+Unfortunately, you're not allowed to sell this game or include it
+in another paid software.
+
+Please refer to the LICENSE file or contact me for more information.
+(You can open a pull request with your question or mention me on twitter [@jeremiecorpinot](https://www.twitter.com/jeremiecorpinot) or the official account [@Brexity2031](https://twitter.com/brexity2031))
